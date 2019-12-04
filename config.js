bdf197391f6151f53764690917e0275c0c0b486c
MarieJung <marie@MarieJungui-MacBookPro.local>
2019-12-04 17:42:45 +0900

    hello

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://yeri-kim.github.io/',
+  title: 'Blog by EunkyoungJung',
+  subtitle: 'hello world',
+  copyright: '',
+  disqusShortname: 'github-io-12',
+  postsPerPage: 6,
+  googleAnalyticsId: 'UA-139726433-1',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    }
+    /*
+    ,
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+    */
+  ],
+  author: {
+    name: 'EunkyoungJung',
+    photo: '/profile.png',
+    bio: '',
+    contacts: {
+      email: 'muchogusto.lol@gmail.com',
+      github: 'EunkyoungJung',
+      linkedIn: 'EunkyoungJung-1028'
+    }
+  }
+};
